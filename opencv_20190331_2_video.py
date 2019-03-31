a563a201d90b3ae770a3ac9af83732db5682a727
aabbcc <iiooiio@hotmail.com>
2019-03-31 18:21:52 +0800

    update file

--- opencv_20190331_2_video.py
+++ opencv_20190331_2_video.py
@@ -2,11 +2,28 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+out = cv2.VideoWriter('out1.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, size,1)
+
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret == True:
+        #frame = cv2.flip(frame,0)
+        
+        out.write(frame)
+    
+        #cv2.imshow('frame', frame)
+        cv2.imshow('gray', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 cap.release()
+out.release()
 cv2.destroyAllWindows()
